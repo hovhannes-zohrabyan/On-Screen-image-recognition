67cc54e6a0a2df10af295c2fde9111d64d562184
Hovhannes <john.zohrabyan@gmail.com>
2019-06-26 17:52:39 +0400

    Mac Adress Change

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import MainController
 import requests
 import uuid
 
+from getmac import get_mac_address
+
+
 
 def run(userId):
     # url = get_url_config()
-    mac_address = get_mac_address()
+    mac_address = get_mac_address(interface="eth0")
+    print(mac_address)
     params = {'userid': userId, 'mac_address': mac_address}
     r = requests.get(url='https://erp.suffescom.com/modules/access/api', params=params)
     json = r.json()
@@ -24,7 +28,7 @@ def get_url_config():
         return url
 
 
-def get_mac_address():
-    mac_hex = iter(hex(uuid.getnode())[2:].zfill(12))
-    mac = ":".join(i + next(mac_hex) for i in mac_hex)
-    return mac
+# def get_mac_address():
+#     mac_hex = iter(hex(uuid.getnode())[2:].zfill(12))
+#     mac = ":".join(i + next(mac_hex) for i in mac_hex)
+#     return mac
